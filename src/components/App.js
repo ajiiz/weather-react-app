52c12a06371034471d66eb544c3ba595a3cb1302
ajiiz <opticpiter@gmail.com>
2020-07-30 13:55:30 +0200

    Added background styling

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React,{ useState }from 'react'
+
+const App = () => {
+    return (
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input
+                        type="text"
+                        className="search-bar"
+                        placeholder="search..."
+                    />
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default App
