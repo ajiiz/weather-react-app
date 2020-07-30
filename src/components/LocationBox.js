2e039ecbefe15464be003a55707ab377a0fd90fb
ajiiz <opticpiter@gmail.com>
2020-07-30 15:08:12 +0200

    Added getDate method

--- src/components/LocationBox.js
+++ src/components/LocationBox.js
@@ -2,10 +2,22 @@ import React, { useEffect } from 'react'
 
 const LocationBox = () => {
 
+    const getDate = () => {
+
+        let date = String(new window.Date())
+        console.log(date)
+        let dayName = date.slice(0,3)
+        let month = date.slice(4,7)
+        let day = date.slice(8,10)
+        let year = new Date().getFullYear()
+
+        return `${dayName}, ${month} ${day} ${year}`
+    }
+
     return (
         <div className="location-box">
             <div className="location">Tychy, Poland</div>
-            <div className="date">Date</div>
+            <div className="date">{getDate()}</div>
         </div>
     )
 }
